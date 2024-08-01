af43b8e87d07b61e381c2c1493ecd739fdcf7179
m7medfathy <melodamea@gmail.com>
2024-08-01 17:43:34 +0300

    comment

--- App.py
+++ App.py
@@ -3,7 +3,6 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from PIL import Image, ImageTk
 # import mysql.connector
-# hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 # my_dp = mysql.connector.connect(
 #     host="localhost",
 #     user="root",
